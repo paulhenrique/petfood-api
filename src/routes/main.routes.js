a5337339cc507ba65e64f674aa0ab2ac92334c22
Paulo Henrique <phvcandido@gmail.com>
2021-05-10 09:31:32 -0300

    Adicionando ultimos detalhes das rotas

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+const Petshop = require("../models/petshop");
+const Product = require("../models/product");
+
+const createSplitTransaction = require("../services/pagarme")
+  .createSplitTransaction;
+
+router.get("/petshops", async (req, res) => {
   try {
-    res.json({ error: false, message: "Hello World" });
+    const petshops = await Petshop.find();
+    res.json({ error: false, petshops });
+  } catch (err) {
+    res.json({ error: true, message: err.message });
+  }
+});
+router.get("/petshops/:id", async (req, res) => {
+  try {
+    const petshop = await Petshop.findById(req.params.id);
+    const product = await Product.find({ petshop_id: petshop.id });
+
+    res.json({ error: false, petshop: { ...petshop._doc, product } });
+  } catch (err) {
+    res.json({ error: true, message: err.message });
+  }
+});
+
+router.post("/purchase", async (req, res) => {
+  try {
+    const transaction = await createSplitTransaction(req.body);
+    res.json(transaction);
   } catch (err) {
     res.json({ error: true, message: err.message });
   }
