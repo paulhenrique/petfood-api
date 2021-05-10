6a08e43f57a2e2a0b7e4580c9e54892847bc0351
Paulo Henrique <phvcandido@gmail.com>
2021-05-09 22:23:01 -0300

    Adicionando arquivos de data para kigrate

---
+++ src/data/addPetshopAndProducts.js
@@ -0,0 +1,33 @@
+const Petshop = require('../models/petshop');
+const Product = require('../models/product');
+const petshops = require('./petfood.json');
+const createRecipients = require('../services/pagarme').createRecipient;
+
+// database
+require('../database');
+
+const addPetshopsAndProducts = async () => {
+  try {
+    for (let petshop of petshops) {
+      const recipient = await createRecipients(petshop.nome);
+
+      if (!recipient.error) {
+        const newPetshop = await new Petshop({
+          ...petshop,
+          recipient_id: recipient.data.id,
+        }).save();
+        await Product.insertMany(
+          petshop.produtos.map((p) => ({ ...p, petshop_id: newPetshop._id }))
+        );
+      } else {
+        console.log(recipient.message);
+      }
+    }
+
+    console.log('Final do Script');
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+addPetshopsAndProducts();
